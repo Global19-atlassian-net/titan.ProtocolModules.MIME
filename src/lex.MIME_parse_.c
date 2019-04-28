1cff000225a777325cb4d8df93a0231f1fb6d2f6
balaskoa <Jeno.Balasko@ericsson.com>
2019-04-28 18:05:30 +0200

    Copyright datum changed for 2000-2019
    Signed-off-by: balaskoa <Jeno.Balasko@ericsson.com>

--- src/lex.MIME_parse_.c
+++ src/lex.MIME_parse_.c
@@ -5740,7 +5740,7 @@ char *MIME_parse_text;
 #line 1 "MIME_parse.l"
 #line 2 "MIME_parse.l"
 /******************************************************************************
-* Copyright (c) 2000-2018 Ericsson Telecom AB
+* Copyright (c) 2000-2019 Ericsson Telecom AB
 * All rights reserved. This program and the accompanying materials
 * are made available under the terms of the Eclipse Public License v2.0
 * which accompanies this distribution, and is available at
